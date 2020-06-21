ac6c3395a14b0c500272d4d8dadcdcf83d281d9c
turaboyh <turabekh@gmail.com>
2020-06-20 20:58:27 -0400

    first commit

---
+++ forms.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
+from wtforms.validators import DataRequired, AnyOf, URL
+from helper import GENRES, STATES
+
+class ShowForm(Form):
+    artist_id = StringField(
+        'artist_id'
+    )
+    venue_id = StringField(
+        'venue_id'
+    )
+    start_time = DateTimeField(
+        'start_time',
+        validators=[DataRequired()],
+        default= datetime.today()
+    )
+
+class VenueForm(Form):
+    name = StringField(
+        'name', validators=[DataRequired()]
+    )
+    city = StringField(
+        'city', validators=[DataRequired()]
+    )
+    state = SelectField(
+        'state', validators=[DataRequired()],
+        choices= STATES
+    )
+    address = StringField(
+        'address', validators=[DataRequired()]
+    )
+    phone = StringField(
+        'phone'
+    )
+    image_link = StringField(
+        'image_link'
+    )
+    genres = SelectMultipleField(
+        # TODO implement enum restriction
+        'genres', validators=[DataRequired()],
+        choices = GENRES
+    )
+    facebook_link = StringField(
+        'facebook_link', validators=[URL()]
+    )
+
+class ArtistForm(Form):
+    name = StringField(
+        'name', validators=[DataRequired()]
+    )
+    city = StringField(
+        'city', validators=[DataRequired()]
+    )
+    state = SelectField(
+        'state', validators=[DataRequired()],
+        choices = STATES
+    )
+    phone = StringField(
+        # TODO implement validation logic for state
+        'phone'
+    )
+    image_link = StringField(
+        'image_link'
+    )
+    genres = SelectMultipleField(
+        # TODO implement enum restriction
+        'genres', validators=[DataRequired()],
+        choices = GENRES
+    )
+    facebook_link = StringField(
+        # TODO implement enum restriction
+        'facebook_link', validators=[URL()]
+    )
+
+# TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
